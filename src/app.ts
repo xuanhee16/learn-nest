ee1051f4eafc012f949a6aff38479edfcd911c1f
xuanhee16 <seonhwa_0713@naver.com>
2022-02-13 22:59:15 +0900

    Feat: 데이터모킹

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import * as express from "express";
-const app: express.Express = express();
-const port: number = 8000;
+import { Cat, CatType } from "./app.model";
 
-app.get("/test", (req: express.Request, res: express.Response) => {
-  res.send("We are the world");
+const app: express.Express = express();
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send({ cats: Cat });
 });
 
-//서버를 여는 역할이 listen
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(8000, () => {
+  console.log("server is on..");
 });
