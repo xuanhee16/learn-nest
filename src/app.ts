ed178ca65ffd7e71a347c24667ffa9d679c5b1c7
xuanhee16 <seonhwa_0713@naver.com>
2022-02-25 20:43:18 +0900

    데이터 create

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 app.get("/cats", (req, res) => {
   try {
     const cats = Cat;
-    throw new Error("db connect error");
+    // throw new Error("db connect error");
     res.status(200).send({ success: true, data: { cats } });
   } catch (error: any) {
     res.status(400).send({ success: false, error: error.message });
@@ -39,6 +39,25 @@ app.get("/cats/:id", (req, res) => {
     res.status(400).send({ success: false, error: error.message });
   }
 });
+
+// Json 파일을 읽을 수 있게 추가
+app.use(express.json());
+
+// 새로운 고양이 추가(새로운 데이터 추가시)
+app.post("/cats", (req, res) => {
+  try {
+    const data = req.body;
+    // console.log(data);
+    // 프론트에서 바디로 요청한 값이 보임
+    // { name: 'yellow', age: 3, species: 'Sharm', isCute: true, friends: [] }
+    Cat.push(data);
+    //그 값을 저장하고 다시 프론트에 보낼수있다.
+    res.status(200).send({ success: true, data: { data } });
+  } catch (error: any) {
+    res.status(400).send({ success: false, error: error.message });
+  }
+});
+
 // 404 middleware
 app.use((req, res, next) => {
   console.log("error middleware");
