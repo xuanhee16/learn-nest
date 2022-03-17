94987942d4829178b1e3f9f982c9531f0642cb34
xuanhee16 <seonhwa_0713@naver.com>
2022-03-17 18:15:52 +0900

    module 구성완료

--- nest/project/src/app.module.ts
+++ nest/project/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { UsersModule } from './users/users.module';
 
 // 여기 있는 모듈들이 최종적으로 main.ts로 이동하게 된다.
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, UsersModule],
   controllers: [AppController], //사용자가 공급받아서 사용
   providers: [AppService], // 공급자가 사업자등록
 })
